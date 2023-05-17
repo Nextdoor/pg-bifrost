cdc2d088d86299e78272f9c6fb87f47ddbafea99
ruban <ruban@nextdoor.com>
2023-05-17 00:10:10 -0700

    Add other k8s related helm scaffolding

---
+++ contrib/Helm.mk
@@ -0,0 +1,129 @@
+# Version 0.1.2
+
+# Pick the chart-name based the name of the directory we are in. This can be
+# overriden - but it picks the "short name" of the directory.
+CHART_NAME ?= $(notdir $(CURDIR))
+
+# By default we look for a `values.local.yaml` file in the directory for the
+# chart when it comes to doing local testing.
+VALUES ?= values.local.yaml
+
+# Target Namespace - this setting is _usually_ overriden.. but for testing of
+# simple charts, the default NS should be fine on a local test cluster.
+NAMESPACE ?= default
+
+# If the NAMESPACE parameter is set to an empty string, then we do not set that
+# flag OR add the --create-namespace flag. This ensures that a chart can be
+# tested against whatever namespace a user is currently logged into. This is
+# valuable for certain kinds of "testing" or "benchmarking" charts where you
+# might want to selectively just run the chart against an existing application.
+ifeq ($(NAMESPACE),default)
+else
+	export HELM_NAMESPACE=$(NAMESPACE)
+	CREATE_NS := --create-namespace
+endif
+
+###############################################################################
+# Construct the final VALUE_ARGS setting that is used to pass into Helm.
+###############################################################################
+VALUE_ARGS := --values values.yaml --values $(VALUES)
+
+###############################################################################
+# The default make target is a non-destructive call
+###############################################################################
+.DEFAULT_GOAL := template
+
+###############################################################################
+# Cleans up the current workspace
+#
+# If you set the optional CLEAN_DEPS_TARGET variable, that target will be
+# called first during the cleanup process.
+###############################################################################
+.PHONY: clean
+clean: $(CLEAN_DEPS_TARGET)
+	rm -rf charts
+
+###############################################################################
+# Optional local dependencies that can be installed.
+#
+# Example - if you need to set up a dependency before your helm chart is run
+# that is specific to a local development experience, you can do it like this
+###############################################################################
+#
+# LOCAL_DEPS_TARGET := install_my_thing
+#
+# install_my_thing:
+# 	kubectl apply -f ....
+#
+# ...
+#
+# include ../Common.mk
+
+
+###############################################################################
+# If the $VALUES file does not exist, then we create it as an empty file. The
+# developer can choose to fill it in if they wish - or not. Helm though requires
+# that all files passed into the `--values` flag must exist.
+###############################################################################
+$(VALUES):
+	touch $(VALUES)
+
+###############################################################################
+# Sets up the dependencies for any subcharts in the charts/* directory
+###############################################################################
+.PHONY: deps
+deps:
+	@echo Pulling dependencies in...
+	helm dependency update .
+
+###############################################################################
+# Shortcut for generating a printed-out template of all the resources that
+# will be created for this target. This is the most useful script for testing
+# helm chart changes and getting the differences from one build to the next.
+###############################################################################
+.PHONY: template
+template: $(VALUES) deps
+	helm template --debug $(VALUE_ARGS) $(CHART_NAME) .
+
+###############################################################################
+# If the LOCAL_DEPS_TARGET variable is set, then before we do the install we
+# will call that target to do any dependency setup. Most of the time this
+# shouldn't be necessary, but in some cases it is.
+#
+# Please document WHY you are setting this if you do.
+###############################################################################
+.PHONY: install
+install: $(LOCAL_DEPS_TARGET) $(VALUES) deps
+	helm install $(VALUE_ARGS) $(CREATE_NS) $(CHART_NAME) .
+
+###############################################################################
+# Once an initial helm chart has been installed, the user should call the
+# upgrade command going forward.
+###############################################################################
+.PHONY: upgrade
+upgrade: $(VALUES)
+	helm upgrade --install $(VALUE_ARGS) $(CHART_NAME) .
+
+###############################################################################
+# See the status of the current Helm deployment for an app
+###############################################################################
+.PHONY: status
+status:
+	helm status $(CHART_NAME)
+
+###############################################################################
+# List the helm charts launched in our namespace
+###############################################################################
+.PHONY: list
+list:
+	helm list
+
+###############################################################################
+# Deleting the charts should fully clean up your environment
+# Delete any installed hooks (e.g. localstack hooks)
+###############################################################################
+.PHONY: uninstall
+uninstall:
+	helm delete $(CHART_NAME)
+	kubectl -n $(NAMESPACE) delete svc,cm,sa,pod,deployment,ingress,job -l 'app.kubernetes.io/managed-by=Helm,app.kubernetes.io/instance=$(CHART_NAME)'
+
---
+++ contrib/Testing.mk
@@ -0,0 +1,135 @@
+# Version 0.1.7
+
+###############################################################################
+# Helpers for installing tooling..
+###############################################################################
+# Quick OS detection so we can pull down the right binaries
+OSNAME         ?= $(shell uname -s | tr A-Z a-z)
+ROOT_DIR       := $(shell git rev-parse --show-toplevel)
+BRANCH_NAME    := $(shell git rev-parse --abbrev-ref HEAD | sed 's/[_-]//g')
+BIN_DIR        ?= $(ROOT_DIR)/bin
+VENV_CMD       := python3 -m venv
+VENV_DIR       := $(ROOT_DIR)/.venv
+VENV_BIN       := $(VENV_DIR)/bin
+
+# This is the standare kind cluster-name that we use for local development.
+KIND_CLUSTER_NAME := default
+
+export PATH    := $(VENV_BIN):$(BIN_DIR):$(PATH)
+
+ifeq ($(OSNAME),linux)
+	GET_HELM_DOCS_URL ?= https://github.com/norwoodj/helm-docs/releases/download/v1.5.0/helm-docs_1.5.0_Linux_x86_64.tar.gz
+else
+	GET_HELM_DOCS_URL ?= https://github.com/norwoodj/helm-docs/releases/download/v1.5.0/helm-docs_1.5.0_Darwin_x86_64.tar.gz
+endif
+
+ifeq ($(HELM_TEST_ALL_CHARTS),true)
+	ALL_FLAG := --all
+endif
+
+HELM_VERSION   ?= v3.7.0
+GET_HELM_URL   ?= https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+GET_CT_URL     ?= https://github.com/helm/chart-testing/releases/download/v3.4.0/chart-testing_3.4.0_$(OSNAME)_amd64.tar.gz
+GET_KIND_URL   ?= https://kind.sigs.k8s.io/dl/v0.11.1/kind-$(OSNAME)-amd64
+
+# First, try to detect helm/ct in the users local env.. if thats there, we'll
+# use it, otherwise fall back to our BIN_DIR which will trigger the Makefile to
+# install the tools.
+HELM           := $(shell which helm || echo $(BIN_DIR)/helm)
+CT             := $(shell which ct || echo $(BIN_DIR)/ct)
+KIND           := $(shell which kind || echo $(BIN_DIR)/kind)
+HELM_DOCS      := $(shell which helm-docs || echo $(BIN_DIR)/helm-docs)
+YAMLLINT       := $(shell which yamllint || echo $(VENV_BIN)/yamllint)
+YAMALE         := $(shell which yamale || echo $(VENV_BIN)/yamale)
+
+###############################################################################
+# Python-based Pre-reqs
+#
+# These are support tools needed by the helm chart-testing tool
+###############################################################################
+.PHONY: venv
+venv: $(VENV_BIN)/activate
+pytools: $(YAMALE) $(YAMLLINT)
+
+$(VENV_BIN)/activate:
+	$(VENV_CMD) $(VENV_DIR)
+
+$(YAMALE): $(VENV_BIN)/activate
+	$(VENV_BIN)/pip3 install yamale==3.0.4
+
+$(YAMLLINT): $(VENV_BIN)/activate
+	$(VENV_BIN)/pip3 install yamllint==1.25.0
+
+###############################################################################
+# Tooling Installation
+#
+# Most or all of these tools should be installed by dotfiles.git.. but we have
+# them here in this Makefile helper just in case.
+###############################################################################
+.PHONY: helm ct tools kind
+helm: $(HELM)
+ct: $(CT) pytools
+kind: $(KIND)
+helm-docs: $(HELM_DOCS)
+tools: $(HELM) $(CT) $(KIND) $(HELM_DOCS) pytools
+
+$(HELM):
+	@echo "Installing $(HELM) from $(GET_HELM_URL)..."
+	@mkdir -p $(BIN_DIR)
+	@curl -fsSL $(GET_HELM_URL) | USE_SUDO=false HELM_INSTALL_DIR=$(BIN_DIR) bash -s -- --version $(HELM_VERSION)
+
+$(CT):
+	@echo "Installing $(CT) from $(GET_CT_URL)..."
+	@mkdir -p $(BIN_DIR)
+	@cd $(BIN_DIR) && curl -fsSL $(GET_CT_URL) | tar -zxv ct && chmod +x ct
+
+$(KIND):
+	@echo "Downloading $(KIND_URL) to $(KIND)..."
+	@mkdir -p $(BIN_DIR)
+	@curl -fsSL -o $(KIND) $(GET_KIND_URL) && chmod +x $(KIND)
+
+$(HELM_DOCS):
+	@echo "Downloading $(GET_HELM_DOCS_URL) to $(HELM_DOCS)..."
+	@mkdir -p $(BIN_DIR)
+	@cd $(BIN_DIR) && curl -fsSL $(GET_HELM_DOCS_URL) | tar -zxv helm-docs && chmod +x helm-docs
+
+###############################################################################
+# Common Chart Testing Targets
+###############################################################################
+.PHONY: _helm_clean
+_helm_clean:
+	@/bin/rm -f charts/*/Chart.lock charts/*/requirements.lock
+
+.PHONY: helm_list_changed
+helm_list_changed:
+	@cd $(ROOT_DIR) && $(CT) list-changed --config $(ROOT_DIR)/ct.yaml \
+			--excluded-charts="$(EXCLUDED_CHARTS_FROM_TESTS)"
+
+.PHONY: helm_lint
+helm_lint: _helm_clean
+	@cd $(ROOT_DIR) && $(CT) lint --config $(ROOT_DIR)/ct.yaml
+
+.PHONY: helm_docs
+helm_docs: $(HELM_DOCS)
+	$(HELM_DOCS)
+	git diff --exit-code
+
+.PHONY: helm_cluster
+helm_cluster: $(KIND)
+	$(KIND) create cluster \
+		--name "$(KIND_CLUSTER_NAME)" \
+		--config "$(ROOT_DIR)/contrib/kind-config.yaml" \
+		--image "kindest/node:v1.21.2" \
+		--wait 60s
+
+.PHONY: helm_test
+helm_test: $(TEST_PREREQS)
+	docker run --rm --network host --name ct \
+		--volume "$(ROOT_DIR)/ct.yaml:/etc/ct/ct.yaml" \
+		--volume "$(ROOT_DIR):/workdir" \
+		--volume "$(HOME)/.kube/config:/root/.kube/config" \
+		--workdir /workdir \
+		quay.io/helmpack/chart-testing:v3.4.0 \
+		ct install \
+			--excluded-charts="$(EXCLUDED_CHARTS_FROM_TESTS)" \
+			$(ALL_FLAG)
