2b97baf1b919ff81a3e9bf5fddbad67b3399e336
ruban <ruban@nextdoor.com>
2023-06-21 10:25:52 -0700

    license 2019 -> 2023

--- transport/transporters/kafka/client_config.yaml.go
+++ transport/transporters/kafka/client_config.yaml.go
@@ -1,5 +1,5 @@
 /*
-  Copyright 2019 Nextdoor.com, Inc.
+  Copyright 2023 Nextdoor.com, Inc.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
