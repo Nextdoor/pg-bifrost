19a785beba48f6100ae13324571ef0a9b7333e4f
Slava Markeyev <slava@nextdoor.com>
2025-06-05 10:43:52 -0700

    fix handling for when column goes from NULL to non-NULL (#140)
    * fix handling for when column goes from NULL to non-NULL
    
    * add unit test
    
    * update expected count

--- itests/tests/base/test_basic/input/001.sql
+++ itests/tests/base/test_basic/input/001.sql
@@ -4,3 +4,10 @@ INSERT INTO customers (first_name, last_name) VALUES ('Hello', 'World');
 INSERT INTO customers (first_name, last_name) VALUES ('Goodbye', 'World');
 UPDATE customers SET last_name = 'Friends' where first_name = 'Hello';
 DELETE FROM customers WHERE first_name = 'Goodbye';
+
+-- Nullable columns
+CREATE TABLE products (id serial PRIMARY KEY, name text, sku INTEGER NULL);
+ALTER TABLE products REPLICA IDENTITY FULL;
+INSERT INTO products (name) VALUES ('widget 1');
+UPDATE products SET sku = 42 where name = 'widget 1';
+UPDATE products SET sku = NULL where name = 'widget 1';
